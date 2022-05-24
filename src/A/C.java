3e7fb120a0257217a426ce19ba93039ff7aabb53
VISHAKA RANI <VISHAKA RANI@user>
2022-05-24 21:30:45 +0530

    commit

---
+++ src/A/C.java
@@ -0,0 +1,8 @@
+package A;
+
+public class C {
+		  public static void main(String[] args) {
+		    System.out.println("Hello World");
+		  }
+		}
+
---
+++ src/A/D.java
@@ -0,0 +1,13 @@
+package A;
+
+public class D {
+	public static void main(String[]args) {
+		byte b;
+		short s=-32768;
+		float f=1.222222f;
+		System.out.println("s Value is "+s);
+		System.out.println("f Value is "+f);
+
+		}
+		}
+
